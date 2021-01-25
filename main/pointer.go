e2337cc76c048c72d41bf09a9721026e34ca4cd1
zuo_h <zuo_hang97@163.com>
2021-01-26 00:11:22 +0800

    go add 指针

---
+++ main/pointer.go
@@ -0,0 +1,39 @@
+package main
+
+/*
+ * Created by zuo_h on 23:30 2021/1/25
+ * 指针示例
+ */
+
+import (
+	"fmt"
+)
+
+func main() {
+	// &取地址符  *int 指针类型
+	i := 23
+	fmt.Println("i 的地址是", &i)
+	var p *int = &i
+	fmt.Printf("p's type is %T. p=%v\n", p, p)
+
+	fmt.Println("p 的地址是", &p)
+	fmt.Println("p 的值是", p)
+	fmt.Println("p 指向的值是", *p)
+
+	//利用指针修改值
+	*p = 64
+	fmt.Println("i 的值是", i)
+
+	//go中的值类型和引用类型
+	//值类型：基本数据类型 int系列、float系列、bool、string、数组、结构体struct
+	//引用类型：指针、slice切片、map、管道chan、interface等
+	//值类型直接存储，一般分配在栈中。引用类型通常在堆区分配（逃逸分析除外）
+
+	//标识符
+	//英文、数字、下划线
+	//数字不可开头
+	//区分大小写
+	//单独的下划线 _ 代表空，可以代表任何一个标识符，但是它的值会被忽略，故它只能做占位符不能做标识符
+	//注意25个关键字
+
+}
--- main/string_to_base_type.go
+++ main/string_to_base_type.go
@@ -14,18 +14,21 @@ func main() {
 	var str1 string = "true"
 	fmt.Printf("str1'type is %T\n", str1)
 	var b bool
-	//strconv.ParseBool()返回两个返回值，但是只想接收一个的时候，可以用下划线代替不想接收的返回值
+	//strconv.ParseBool()返回两个返回值num,err.但是只想接收一个的时候，可以用下划线代替不想接收的返回值
 	b, _ = strconv.ParseBool(str1)
 	fmt.Printf("b'type is %T. b=%v\n", b, b)
 
 	//string to int
 	var str2 = "22"
 	parseInt, _ := strconv.ParseInt(str2, 10, 64)
-	fmt.Printf("parseInt'type is %T. b=%v\n", parseInt, parseInt)
+	fmt.Printf("parseInt'type is %T. parseInt=%v\n", parseInt, parseInt)
 
 	//string to float
 	var str3 = "3.14"
 	float, _ := strconv.ParseFloat(str3, 64)
-	fmt.Printf("float'type is %T. float=%v", float, float)
+	fmt.Printf("float'type is %T. float=%v\n", float, float)
 
+	//go 在数据类型转换的时候，要是不是一个有效数字，则会将转换后的数字类型的默认值赋值给结果
+	parseInt1, _ := strconv.ParseInt(str1, 10, 64)
+	fmt.Printf("parseInt1'type is %T. parseInt1=%v\n", parseInt1, parseInt1)
 }
