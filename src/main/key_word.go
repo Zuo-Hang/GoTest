d7ac92236ca27838aad12552ef264b4f40f26e4a
zuo_h <zuo_hang97@163.com>
2021-01-28 00:24:18 +0800

    关键字 & 运算符

---
+++ src/main/key_word.go
@@ -0,0 +1,27 @@
+package main
+
+/*
+ * Created by zuo_h on 23:14 2021/1/27
+ */
+//关键字 25个
+//·程序声明：import、package
+//·程序实体声明和定义：chan、const、func、interface、map、struct、type、var
+//·程序流程控制：go、select、break、case、continue、default、defer、else、fallthrough、for、goto、if、range、return、switch
+
+//var和const ：变量和常量的声明
+//var varName type  或者 varName : = value
+//package and import: 导入
+//func： 用于定义函数和方法
+//return ：用于从函数返回
+//defer someCode ：在函数退出之前执行
+//go : 用于并行
+//select 用于选择不同类型的通讯
+//interface 用于定义接口
+//struct 用于定义抽象数据类型
+//break、case、continue、for、fallthrough、else、if、switch、goto、default 流程控制
+//chan用于channel通讯
+//type用于声明自定义类型
+//map用于声明map类型数据
+//range用于读取slice、map、channel数据
+
+//系统预定义标识符  数据类型+内嵌函数
---
+++ src/main/operator.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+/*
+ * Created by zuo_h on 23:18 2021/1/27
+ */
+
+//运算符 六种 ：算术运算 赋值运算 比较运算（关系运算） 逻辑运算 位运算 其他运算（& *） 在go中没有三元运算符
+func arithmetic() {
+	//算术运算符：+ - * / % ++ -- +（字符串拼接）
+	fmt.Println(10 / 3)
+	fmt.Println(10 % 4)
+	fmt.Println(10 % -3)
+	var i int = 10
+	//在Go中 ++ -- 只能当作一个独立的语言来使用 不能 k:=i++ 这样组合使用
+	//Go中的 ++ -- 只能跟在变量后 不存在 ++1 这种前置自增
+	i++
+	fmt.Println(i)
+}
+
+func relationship() {
+	//关系运算符
+	// ==  !=  >  <  >=  <=
+	b := 7 > 8
+	fmt.Println(b)
+}
+
+func logic() {
+	//逻辑运算符：将多个关系运算链接
+	//&& || !  短路
+	var age = 30
+	if age > 20 && age < 40 {
+		fmt.Println(strconv.Itoa(age) + " between 20 and 40")
+	}
+	if age > 20 || age < 10 {
+		fmt.Println(strconv.Itoa(age) + " big than 20 or small than 10")
+	}
+
+}
+
+func assignment() {
+	//赋值运算符
+	// = 符合运算符 += -= *= /= %=
+	var a = 10
+	var b = 20
+	fmt.Printf("a=%v,b=%v\n", a, b)
+	a = a + b
+	b = a - b
+	a = a - b
+	fmt.Printf("a=%v,b=%v\n", a, b)
+}
+
+func main() {
+	arithmetic()
+	relationship()
+	logic()
+	assignment()
+}
